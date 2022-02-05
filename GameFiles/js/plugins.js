6924c2823f1aeec42617ca5b040491cadb894cca
Bailey Danyluk <baileydanyluk@gmail.com>
2022-02-05 14:38:00 -0700

    add local events for time freeze

--- GameFiles/js/plugins.js
+++ GameFiles/js/plugins.js
@@ -3,6 +3,6 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"GALV_EventSpawner","status":true,"description":"(v.1.8) Spawn events from a specified spawn map to a desired location.","parameters":{"Spawn Map Id":"1"}},
+{"name":"GALV_EventSpawner","status":true,"description":"(v.1.8) Spawn events from a specified spawn map to a desired location.","parameters":{"Spawn Map Id":"2"}},
 {"name":"TimeStop","status":true,"description":"Controls the spawning of time-freeze bubbles","parameters":{}}
 ];
