528471bb7200b5178920240adf5b2c6fdb565f93
Bailey Danyluk <baileydanyluk@gmail.com>
2022-02-25 19:45:04 -0700

    Change game menu

--- GameFiles/js/plugins/AltMenuScreen.js
+++ GameFiles/js/plugins/AltMenuScreen.js
@@ -17,76 +17,85 @@
  */
 
 (function() {
+    function Window_Hint() {
+        this.initialize.apply(this, arguments);
+    }
+    
+    Window_Hint.prototype = Object.create(Window_Command.prototype);
+    Window_Hint.prototype.constructor = Window_Hint;
+    
+    Window_Hint.prototype.initialize = function(x, y) {
+        Window_Command.prototype.initialize.call(this, x, y);
+    };
 
-    var _Scene_Menu_create = Scene_Menu.prototype.create;
+    Window_Hint.prototype.makeCommandList = function() {
+        this.addCommand("Your Hint Here", 'gameHint', true);
+    };
+
+    function Scene_Hint() {
+        this.initialize.apply(this, arguments);
+    }
+    
+    Scene_Hint.prototype = Object.create(Scene_MenuBase.prototype);
+    Scene_Hint.prototype.constructor = Scene_Hint;
+    
+    Scene_Hint.prototype.initialize = function() {
+        Scene_MenuBase.prototype.initialize.call(this);
+    };
+    
+    Scene_Hint.prototype.create = function() {
+        Scene_MenuBase.prototype.create.call(this);
+
+        this._hintWindow = new Window_Hint(0, 0);
+        this._hintWindow.setHandler('gameHint', this.popScene.bind(this));
+
+        this.addWindow(this._hintWindow);
+    };
+
+    /* ------------------------------------- */
+
+    Window_Options.prototype.addGeneralOptions = function() {
+        this.addCommand(TextManager.commandRemember, 'commandRemember');
+    };
+
     Scene_Menu.prototype.create = function() {
-        _Scene_Menu_create.call(this);
-        this._statusWindow.x = 0;
-        this._statusWindow.y = this._commandWindow.height;
-        this._goldWindow.x = Graphics.boxWidth - this._goldWindow.width;
+        Scene_MenuBase.prototype.create.call(this);
+        this.createCommandWindow();
     };
 
-    Window_MenuCommand.prototype.windowWidth = function() {
-        return Graphics.boxWidth;
+    Scene_Menu.prototype.start = function() {
+        Scene_MenuBase.prototype.start.call(this);
     };
 
-    Window_MenuCommand.prototype.maxCols = function() {
-        return 4;
+    Window_MenuCommand.prototype.addOriginalCommands = function() {
+        this.addCommand("Request Hint", 'hint', true);
     };
 
-    Window_MenuCommand.prototype.numVisibleRows = function() {
-        return 2;
+    Window_MenuCommand.prototype.makeCommandList = function() {
+        this.addOriginalCommands();
+        this.addOptionsCommand();
+        this.addSaveCommand();
+        this.addGameEndCommand();
     };
 
-    Window_MenuStatus.prototype.windowWidth = function() {
-        return Graphics.boxWidth;
+    Scene_Menu.prototype.createCommandWindow = function() {
+        this._commandWindow = new Window_MenuCommand(0, 0);
+        this._commandWindow.setHandler('hint',      this.hint.bind(this));
+        this._commandWindow.setHandler('options',   this.commandOptions.bind(this));
+        this._commandWindow.setHandler('save',      this.commandSave.bind(this));
+        this._commandWindow.setHandler('gameEnd',   this.commandGameEnd.bind(this));
+        this._commandWindow.setHandler('cancel',    this.popScene.bind(this));
+
+        let centerX = Graphics.width / 2;
+        let centerY = Graphics.height / 2;
+
+        this._commandWindow.move(centerX - this._commandWindow._width / 2, centerY - this._commandWindow._height / 2, this._commandWindow._width, this._commandWindow._height);
+
+        this.addWindow(this._commandWindow);
     };
 
-    Window_MenuStatus.prototype.windowHeight = function() {
-        var h1 = this.fittingHeight(1);
-        var h2 = this.fittingHeight(2);
-        return Graphics.boxHeight - h1 - h2;
-    };
-
-    Window_MenuStatus.prototype.maxCols = function() {
-        return 4;
-    };
-
-    Window_MenuStatus.prototype.numVisibleRows = function() {
-        return 1;
-    };
-
-    Window_MenuStatus.prototype.drawItemImage = function(index) {
-        var actor = $gameParty.members()[index];
-        var rect = this.itemRectForText(index);
-        var w = Math.min(rect.width, 144);
-        var h = Math.min(rect.height, 144);
-        var lineHeight = this.lineHeight();
-        this.changePaintOpacity(actor.isBattleMember());
-        this.drawActorFace(actor, rect.x, rect.y + lineHeight * 2.5, w, h);
-        this.changePaintOpacity(true);
-    };
-
-    Window_MenuStatus.prototype.drawItemStatus = function(index) {
-        var actor = $gameParty.members()[index];
-        var rect = this.itemRectForText(index);
-        var x = rect.x;
-        var y = rect.y;
-        var width = rect.width;
-        var bottom = y + rect.height;
-        var lineHeight = this.lineHeight();
-        this.drawActorName(actor, x, y + lineHeight * 0, width);
-        this.drawActorLevel(actor, x, y + lineHeight * 1, width);
-        this.drawActorClass(actor, x, bottom - lineHeight * 4, width);
-        this.drawActorHp(actor, x, bottom - lineHeight * 3, width);
-        this.drawActorMp(actor, x, bottom - lineHeight * 2, width);
-        this.drawActorIcons(actor, x, bottom - lineHeight * 1, width);
-    };
-
-    var _Window_MenuActor_initialize = Window_MenuActor.prototype.initialize;
-    Window_MenuActor.prototype.initialize = function() {
-        _Window_MenuActor_initialize.call(this);
-        this.y = this.fittingHeight(2);
+    Scene_Menu.prototype.hint = function() {
+        SceneManager.push(Scene_Hint);
     };
 
 })();
